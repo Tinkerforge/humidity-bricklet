74b92fb018b58f3e8afaac7d2caadd4d8657cb9a
Ishraq Ibne Ashraf <ishraq@tinkerforge.com>
2014-02-12 13:41:50 +0100

    perl: removed unwanted callback function

--- software/examples/perl/example_simple.pl
+++ software/examples/perl/example_simple.pl
@@ -10,12 +10,6 @@ use constant UID => '7bA'; # Change to your UID
 my $ipcon = Tinkerforge::IPConnection->new(); # Create IP connection
 my $h = Tinkerforge::BrickletHumidity->new(&UID, $ipcon); # Create device object
 
-# Callback function for humidity callback (parameter has unit %RH/10)
-sub cb_humidity
-{
-    my ($rh) = @_;
-    print "\nRelative Humidity: ".$rh/10.0." RH%\n";
-}
 $ipcon->connect(&HOST, &PORT); # Connect to brickd
 # Don't use device before ipcon is connected
 
