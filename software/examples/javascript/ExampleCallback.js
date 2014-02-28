d832fc11832cb5ff3b0ce86f0871255cb4d90502
Matthias Bolte <matthias@tinkerforge.com>
2014-02-28 19:16:05 +0100

    javascript: Break long lines and cleanup whitespace

--- software/examples/javascript/ExampleCallback.js
+++ software/examples/javascript/ExampleCallback.js
@@ -2,22 +2,22 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = '7bA';// Change to your UID
+var UID = '7bA'; // Change to your UID
 
-var ipcon = new Tinkerforge.IPConnection();// Create IP connection
-var h = new Tinkerforge.BrickletHumidity(UID, ipcon);// Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var h = new Tinkerforge.BrickletHumidity(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
-
+); // Connect to brickd
 // Don't use device before ipcon is connected
+
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Set Period for rh callback to 1s (1000ms)
-        // Note: The callback is only called every second if the 
+        // Note: The callback is only called every second if the
         // humidity has changed since the last call!
         h.setHumidityCallbackPeriod(1000);
     }
@@ -29,7 +29,7 @@ h.on(Tinkerforge.BrickletHumidity.CALLBACK_HUMIDITY,
     function(humidity) {
         console.log('Relative Humidity: '+humidity/10+' %RH');
     }
-); 
+);
 
 console.log("Press any key to exit ...");
 process.stdin.on('data',
@@ -38,4 +38,3 @@ process.stdin.on('data',
         process.exit(0);
     }
 );
-
--- software/examples/javascript/ExampleThreshold.js
+++ software/examples/javascript/ExampleThreshold.js
@@ -2,24 +2,24 @@ var Tinkerforge = require('tinkerforge');
 
 var HOST = 'localhost';
 var PORT = 4223;
-var UID = '7bA';//Change to your UID
+var UID = '7bA'; // Change to your UID
 
-var ipcon = new Tinkerforge.IPConnection();//Create IP connection
-var h = new Tinkerforge.BrickletHumidity(UID, ipcon);//Create device object
+var ipcon = new Tinkerforge.IPConnection(); // Create IP connection
+var h = new Tinkerforge.BrickletHumidity(UID, ipcon); // Create device object
 
 ipcon.connect(HOST, PORT,
     function(error) {
-        console.log('Error: '+error);        
+        console.log('Error: '+error);
     }
-);// Connect to brickd
+); // Connect to brickd
+// Don't use device before ipcon is connected
 
-//Don't use device before ipcon is connected
 ipcon.on(Tinkerforge.IPConnection.CALLBACK_CONNECTED,
     function(connectReason) {
         // Set threshold callbacks with a debounce time of 10 seconds (10000ms)
         h.setDebouncePeriod(10000);
         // Configure threshold for "outside of 30 to 60 %RH" (unit is %RH/10)
-        h.setHumidityCallbackThreshold('o', 30*10, 60*10);    
+        h.setHumidityCallbackThreshold('o', 30*10, 60*10);
     }
 );
 
@@ -43,4 +43,3 @@ process.stdin.on('data',
         process.exit(0);
     }
 );
-
