7c14fda83a8930bd3a801cd6fdebfcbdbf6f3298
Matthias Bolte <matthias@tinkerforge.com>
2015-08-13 20:38:52 +0200

    Use correct simple value type

--- software/src/config.h
+++ software/src/config.h
@@ -45,12 +45,13 @@
 #define BRICKLET_HAS_SIMPLE_SENSOR
 #define BRICKLET_NO_OFFSET
 #define INVOCATION_IN_BRICKLET_CODE
+#define SIMPLE_VALUE_TYPE uint16_t
 #define NUM_SIMPLE_VALUES 2
 
 typedef struct {
 	int32_t value[NUM_SIMPLE_VALUES];
 	int32_t last_value[NUM_SIMPLE_VALUES];
-	int16_t humidity_avg;
+	uint16_t humidity_avg;
 	int32_t humidity_avg_sum;
 
 	uint32_t signal_period[NUM_SIMPLE_VALUES];
