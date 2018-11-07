f5aa32814b84a1149aef0218fa55b3da1aaaa4ab
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:25:56 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -15,10 +15,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     // Get threshold receivers with a debounce time of 10 seconds (10000ms).
     h.set_debounce_period(10000);
 
-    // Create receiver for humidity reached events.
-    let humidity_reached_receiver = h.get_humidity_reached_receiver();
+    let humidity_reached_receiver = h.get_humidity_reached_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `h` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `h` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for humidity_reached in humidity_reached_receiver {
