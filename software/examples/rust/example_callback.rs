919332718e9c622ce0480856aedeccb7a9b9bd49
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 16:21:22 +0100

    rust: Accept host names for ip connection, fix naming inconsistencies

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,31 +1,32 @@
 use std::{error::Error, io, thread};
-use tinkerforge::{humidity_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{humidity_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Humidity Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Humidity Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let humidity_bricklet = HumidityBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let h = HumidityBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    //Create listener for humidity events.
-    let humidity_listener = humidity_bricklet.get_humidity_receiver();
-    // Spawn thread to handle received events. This thread ends when the humidity_bricklet
+    // Create receiver for humidity events.
+    let humidity_receiver = h.get_humidity_receiver();
+
+    // Spawn thread to handle received events. This thread ends when the `h` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
-        for event in humidity_listener {
-            println!("Humidity: {}{}", event as f32 / 10.0, " %RH");
+        for humidity in humidity_receiver {
+            println!("Humidity: {} %RH", humidity as f32 / 10.0);
         }
     });
 
-    // Set period for humidity listener to 1s (1000ms)
+    // Set period for humidity receiver to 1s (1000ms).
     // Note: The humidity callback is only called every second
     //       if the humidity has changed since the last call!
-    humidity_bricklet.set_humidity_callback_period(1000);
+    h.set_humidity_callback_period(1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,21 +1,21 @@
 use std::{error::Error, io};
 
-use tinkerforge::{humidity_bricklet::*, ipconnection::IpConnection};
+use tinkerforge::{humidity_bricklet::*, ip_connection::IpConnection};
 
-const HOST: &str = "127.0.0.1";
+const HOST: &str = "localhost";
 const PORT: u16 = 4223;
-const UID: &str = "XYZ"; // Change XYZ to the UID of your Humidity Bricklet
+const UID: &str = "XYZ"; // Change XYZ to the UID of your Humidity Bricklet.
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let ipcon = IpConnection::new(); // Create IP connection
-    let humidity_bricklet = HumidityBricklet::new(UID, &ipcon); // Create device object
+    let ipcon = IpConnection::new(); // Create IP connection.
+    let h = HumidityBricklet::new(UID, &ipcon); // Create device object.
 
-    ipcon.connect(HOST, PORT).recv()??; // Connect to brickd
-                                        // Don't use device before ipcon is connected
+    ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
+                                          // Don't use device before ipcon is connected.
 
-    // Get current humidity
-    let humidity = humidity_bricklet.get_humidity().recv()?;
-    println!("Humidity: {}{}", humidity as f32 / 10.0, " %RH");
+    // Get current humidity.
+    let humidity = h.get_humidity().recv()?;
+    println!("Humidity: {} %RH", humidity as f32 / 10.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
