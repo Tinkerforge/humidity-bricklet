56d3b675aa61af366e552426d63bc5e8b921f1ef
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:34:07 +0100

    rust: Reformat examples.

--- software/examples/rust/example_callback.rs
+++ software/examples/rust/example_callback.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, io, thread};
-use tinkerforge::{humidity_bricklet::*, ip_connection::IpConnection};
+use std::{io, error::Error};
+use std::thread;
+use tinkerforge::{ip_connection::IpConnection, 
+                  humidity_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,23 +13,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let h = HumidityBricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    let humidity_receiver = h.get_humidity_callback_receiver();
+     let humidity_receiver = h.get_humidity_callback_receiver();
 
-    // Spawn thread to handle received callback messages.
-    // This thread ends when the `h` object
-    // is dropped, so there is no need for manual cleanup.
-    thread::spawn(move || {
-        for humidity in humidity_receiver {
-            println!("Humidity: {} %RH", humidity as f32 / 10.0);
-        }
-    });
+        // Spawn thread to handle received callback messages. 
+        // This thread ends when the `h` object
+        // is dropped, so there is no need for manual cleanup.
+        thread::spawn(move || {
+            for humidity in humidity_receiver {           
+                		println!("Humidity: {} %RH", humidity as f32 /10.0);
+            }
+        });
 
-    // Set period for humidity receiver to 1s (1000ms).
-    // Note: The humidity callback is only called every second
-    //       if the humidity has changed since the last call!
-    h.set_humidity_callback_period(1000);
+		// Set period for humidity receiver to 1s (1000ms).
+		// Note: The humidity callback is only called every second
+		//       if the humidity has changed since the last call!
+		h.set_humidity_callback_period(1000);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
